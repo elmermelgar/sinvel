4e96660bc6fd2217ee154eef85aa0009b5f2f215
Oscar_Rivas <oscar.desarrollador@gmail.com>
2017-06-04 10:55:00 -0600

    Ubicacion de Vehiculo en bodega (2)

--- sinvel/routes/oscar.py
+++ sinvel/routes/oscar.py
@@ -1,3 +1,5 @@
 def includeme(config):
     config.add_route('registroImportacion', '/registro_importacion')
     config.add_route('registroImportacionGuardar', '/reg_import_guardar')
+    config.add_route('ubiVehSeleccionar', '/uv_sel_ubicacion/{idv}')
+    config.add_route('ubiVehGuardar', '/uv_guardar_ubicacion/{idv}/{idu}')
